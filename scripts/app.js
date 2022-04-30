796e061e8fa1f685d28fd8a705ffbff96593d3a2
Sam <samwentupp@outlook.com>
2022-04-29 20:06:59 -0700

    js logic for button

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,14 @@
-console.log("hello world"); 
+let health = 100;
+
+window.addEventListener('load', () => {
+    title_btn = document.getElementsByClassName('Title-Btn')[0]; 
+
+    title_btn.addEventListener('click', () => {
+        health = health - 20; 
+        console.log(health); 
+
+        if (health <= 0){
+            window.location.href = "page_1.html"
+        }
+    })
+})
