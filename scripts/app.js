52f30082c122ac0774186e37a82df8e5d7019763
Sam <samwentupp@outlook.com>
2022-04-29 20:31:32 -0700

    health bar

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,13 @@
 let health = 100;
 
 window.addEventListener('load', () => {
-    title_btn = document.getElementsByClassName('Title-Btn')[0]; 
+    title_btn = document.getElementsByClassName('Title-Btn')[0];
+    green_bar = document.getElementsByClassName('Green-Bar')[0];  
 
     title_btn.addEventListener('click', () => {
         health = health - 20; 
         console.log(health); 
+        green_bar.style.width = health+ "%"
 
         if (health <= 0){
             window.location.href = "page_1.html"
